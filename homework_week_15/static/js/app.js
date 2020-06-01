fc0fcc12e5cf1901f45bb534b248ccaeb4ec5751
jasonfung <jfung313@gmail.com>
2020-05-31 21:41:35 -0700

    v4

--- homework_week_15/static/js/app.js
+++ homework_week_15/static/js/app.js
@@ -3,8 +3,10 @@
 function gatherData () {
     d3.json('samples.json').then(function(data) {
         console.log(data);
-        console.log("All data has been stored into variable 'allData.'")
-        console.log('Update 9:13')
+        var names = data.names;
+        console.log(names);
+        var otuIDs = data.samples.map(row => row);
+        console.log(otuIDs);
     });
 };
 

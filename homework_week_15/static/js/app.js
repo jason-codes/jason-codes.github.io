69b1580cfdc12c964a764bdfaff65346c2267dd9
jasonfung <jfung313@gmail.com>
2020-05-31 22:01:51 -0700

    v7

--- homework_week_15/static/js/app.js
+++ homework_week_15/static/js/app.js
@@ -3,12 +3,14 @@
 function gatherData () {
     d3.json('samples.json').then(function(data) {
         console.log(data);
-        var names = data.names;
+        var names = data.samples.map(row => row.id);
         console.log(names);
-        var otuIDs = data.samples.map(row => row);
+        var otuIDs = data.samples.map(row => row.otu_ids);
         console.log(otuIDs);
-        var names2 = data.samples.map(row => row.id);
-        console.log(names2);
+        var otuValues = data.samples.map(row => row.sample_values);
+        console.log(otuValues);
+        var otuLabels = data.samples.map(row => row.otu_labels);
+        console.log(otuLables);
     });
 };
 

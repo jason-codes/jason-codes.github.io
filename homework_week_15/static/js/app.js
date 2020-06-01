edf91d1db441dcefef97bbb5fc520b6ab87fba3a
jasonfung <jfung313@gmail.com>
2020-05-31 21:14:09 -0700

    v3

--- homework_week_15/static/js/app.js
+++ homework_week_15/static/js/app.js
@@ -1,7 +1,11 @@
-console.log('Hello');
+// Create data gathering function
 
-d3.json('samples.json').then(function(data) {
-    console.log(data);
-    console.log("All data has been stored into variable 'allData.'")
-});
+function gatherData () {
+    d3.json('samples.json').then(function(data) {
+        console.log(data);
+        console.log("All data has been stored into variable 'allData.'")
+        console.log('Update 9:13')
+    });
+};
 
+gatherData();

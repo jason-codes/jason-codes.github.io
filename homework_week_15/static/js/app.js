89783a80cba096534eb8e3f0188fdf400cdf9e4b
jasonfung <jfung313@gmail.com>
2020-05-31 22:52:44 -0700

    v10

--- homework_week_15/static/js/app.js
+++ homework_week_15/static/js/app.js
@@ -2,14 +2,14 @@
 
 function gatherData () {
     d3.json('samples.json').then(function(data) {
-        var index = 0
-        var names = data.samples.map(row => row.id[index]);
+        console.log(data);
+        var names = data.samples.map(row => row.id);
         console.log(names);
-        var otuIDs = data.samples.map(row => row.otu_ids[index]);
+        var otuIDs = data.samples.map(row => row.otu_ids);
         console.log(otuIDs);
-        var otuValues = data.samples.map(row => row.sample_values[index]);
+        var otuValues = data.samples.map(row => row.sample_values);
         console.log(otuValues);
-        var otuLabels = data.samples.map(row => row.otu_labels[index]);
+        var otuLabels = data.samples.map(row => row.otu_labels);
         console.log(otuLabels);
     });
 };

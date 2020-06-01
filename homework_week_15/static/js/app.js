4c8fc3a8f290aa012aefd3e19aed1e036d5f61b7
jasonfung <jfung313@gmail.com>
2020-05-31 20:06:35 -0700

    version8

--- homework_week_15/static/js/app.js
+++ homework_week_15/static/js/app.js
@@ -1,10 +1,9 @@
 // Read data from samples.json file
 
-function gatherData() {
-    d3.json('../../samples.json').then(function(data) {
-        var all_data = data
-        console.log(all_data);
-    });
-};
+
+d3.json('../../samples.json').then(function(data) {
+    var all_data = data;
+    console.log(all_data);
+});
 
 gatherData();
